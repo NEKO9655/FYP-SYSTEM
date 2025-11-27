8dee0f9957b8eb0d0d980765f9b7ec5a5254f536
NEKO9655 <jiaqi3616@gmail.com>
2025-11-27 10:37:52 +0800

    Phase 3:backend API

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    first_name = None
+    last_name = None
+
     ROLE_CHOICES = (
         ('student', 'Student'),
         ('lecturer', 'Lecturer'),
         ('coordinator', 'Coordinator'),
     )
+
+    full_name = models.CharField(max_length=255, blank=True, verbose_name="Full Name (as per NRIC)")
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
 
     def get_full_name(self):
-        if self.first_name and self.last_name:
-            return f"{self.first_name} {self.last_name}"
-        return self.username
+        return self.full_name or self.username
 
     def __str__(self):
         return self.get_full_name()
