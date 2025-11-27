b4943fdfc2bc4e49718ceabc83fdb7a232669004
NEKO9655 <jiaqi3616@gmail.com>
2025-11-27 12:15:45 +0800

    Phase 3:backend API

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class Profile(models.Model):
     ROLE_CHOICES = (
@@ -14,12 +12,7 @@ class Profile(models.Model):
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
 
     def __str__(self):
-        return self.user.get_full_name() or self.user.username
-
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+        return self.user.username
 
 class FYPProject(models.Model):
     student = models.OneToOneField(User, on_delete=models.CASCADE, limit_choices_to={'profile__role': 'student'})
