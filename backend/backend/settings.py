c4f57a23f8111cce8a1ee8e668d520739da83e8f
NEKO9655 <jiaqi3616@gmail.com>
2025-12-24 13:04:42 +0800

    Phase 3:backend API

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,6 +85,10 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+]
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
